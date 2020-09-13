279549d14ee11ff78074f7d40dabddf128760f53
4Applepen <37585942+4Applepen@users.noreply.github.com>
2020-09-13 22:58:05 +0200

    Update instant_install_ext_module.py

--- userbot/plugins/instant_install_ext_module.py
+++ userbot/plugins/instant_install_ext_module.py
@@ -1,47 +1,68 @@
-#   Copyright 2019 - 2020 DarkPrinc3
-
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-
-#       http://www.apache.org/licenses/LICENSE-2.0
-
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-
-from userbot import bot
-from telethon import events
-from telethon import functions, types
-from telethon.tl.types import InputMessagesFilterDocument
-from userbot.utils import command, remove_plugin, load_module
-from var import Var
-from pathlib import Path
-from userbot import LOAD_PLUG
-import sys
-import asyncio
+from telethon import events, errors, functions, types
+import inspect
 import traceback
-import os
-import userbot.utils
+import asyncio
+import sys
+import io
 
-@command(pattern="^.extdl", outgoing=True)
-async def install(event):
+
+@command(pattern="^.exec")
+async def _(event):
     if event.fwd_from:
         return
-    chat = Var.PLUGIN_CHANNEL
-    documentss = await borg.get_messages(chat, None , filter=InputMessagesFilterDocument)
-    total = int(documentss.total)
-    total_doxx = range(0, total)
-    await event.delete()
-    for ixo in total_doxx:
-        mxo = documentss[ixo].id
-        downloaded_file_name = await event.client.download_media(await borg.get_messages(chat, ids=mxo), "userbot/plugins/")
-        if "(" not in downloaded_file_name:
-            path1 = Path(downloaded_file_name)
-            shortname = path1.stem
-            load_module(shortname.replace(".py", ""))
-            await borg.send_message(event.chat_id, "Plugin instato `{}` con successo.".format(os.path.basename(downloaded_file_name)))
-        else:
-            await borg.send_message(event.chat_id, "Plugin `{}` è già installato o pre installato..".format(os.path.basename(downloaded_file_name)))
+    await event.edit("Processing ...")
+    cmd = event.text.split(" ", maxsplit=1)[1]
+    reply_to_id = event.message.id
+    if event.reply_to_msg_id:
+        reply_to_id = event.reply_to_msg_id
+
+    old_stderr = sys.stderr
+    old_stdout = sys.stdout
+    redirected_output = sys.stdout = io.StringIO()
+    redirected_error = sys.stderr = io.StringIO()
+    stdout, stderr, exc = None, None, None
+
+    try:
+        await aexec(cmd, event)
+    except Exception:
+        exc = traceback.format_exc()
+
+    stdout = redirected_output.getvalue()
+    stderr = redirected_error.getvalue()
+    sys.stdout = old_stdout
+    sys.stderr = old_stderr
+
+    evaluation = ""
+    if exc:
+        evaluation = exc
+    elif stderr:
+        evaluation = stderr
+    elif stdout:
+        evaluation = stdout
+    else:
+        evaluation = "Success"
+
+    final_output = "**EXEC**: `{}` \n\n **OUTPUT**: \n`{}` \n".format(cmd, evaluation)
+
+    if len(final_output) > 4096:
+        with io.BytesIO(str.encode(final_output)) as out_file:
+            out_file.name = "eval.text"
+            await bot.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption=f"**PROCCESSED**: `{cmd}`",
+                reply_to=reply_to_id
+            )
+            await event.delete()
+    else:
+        await event.edit(final_output)
+
+
+async def aexec(code, event):
+    exec(
+        f'async def __aexec(event): ' +
+        ''.join(f'\n {l}' for l in code.split('\n'))
+    )
+    return await locals()['__aexec'](event)
